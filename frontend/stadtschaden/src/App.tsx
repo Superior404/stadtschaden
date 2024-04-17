d460ed41f9c9be87cfd8e9795e13744e47949de9
Superior404 <93605138+Superior404@users.noreply.github.com>
2024-04-17 18:58:03 +0200

    added frontend, tailwind and removed unnecessary folder/files

---
+++ frontend/stadtschaden/src/App.tsx
@@ -0,0 +1,15 @@
+import LandingPage from "./components/screens/LandingPage";
+import ContactPage from "./components/screens/ContactPage";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="mt-4 ml-4">This will be an incredible Stadtschaden page, u wont believe</h1>
+      </div>
+      <p className="ml-4">Crazy oder</p>
+    </>
+  );
+}
+
+export default App;
