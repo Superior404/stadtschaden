9916106fa87a4847bfc6e15d8c51ee9a7b78c6e2
Superior404 <93605138+Superior404@users.noreply.github.com>
2024-04-25 19:28:34 +0200

    NavBar is now responsive

--- frontend/stadtschaden/src/components/common/Nav.tsx
+++ frontend/stadtschaden/src/components/common/Nav.tsx
@@ -1,35 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { navLinks } from "../../constants/NavLinks";
 import stadtschadenLogo from "../../assets/stadtschaden-logo.png";
 import { Link, NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const Nav = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  const toggleNav = () => {
+    setShowNav(!showNav);
+  };
+
   return (
-    <header className="flex justify-center items-center mt-10 ">
-      <Link className="absolute left-0 ml-16" to="">
-        <img src={stadtschadenLogo} alt="Logo" width={320} />
+    <header className="mt-10">
+      <Link to="">
+        <img
+          src={stadtschadenLogo}
+          alt="Logo"
+          className="absolute 2xl:w-64 xl:w-56 lg:w-48 w-48 ml-8 lg:ml-16 2xl:ml-24 -mt-1"
+        />
       </Link>
-      <nav className="flex justify-center w-full">
-        <div className="">
-          <ul className="flex flex-row justify-center items-center gap-12">
-            {navLinks.map((link) => (
-              <li key={link.label}>
-                <NavLink
-                  className={({ isActive }) =>
-                    "font-palanquin font-semibold text-xl " +
-                    (isActive
-                      ? "text-primary"
-                      : "text-white")
-                  }
-                  to={link.to}
-                >
-                  {link.label}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </nav>
+      <div className="lg:hidden flex justify-end mr-8 mb-2">
+        <button onClick={toggleNav}>
+          {showNav ? (
+            <FontAwesomeIcon icon={faTimes} className="text-2xl text-white" />
+          ) : (
+            <FontAwesomeIcon icon={faBars} className="text-2xl text-white" />
+          )}
+        </button>
+      </div>
+      <ul
+        className={`lg:flex flex-1 ${
+          showNav ? "flex" : "hidden"
+        } flex-col lg:justify-center lg:items-center lg:flex-row 2xl:gap-10 xl:gap-7 gap-4`}
+      >
+        {navLinks.map((link) => (
+          <li key={link.label} className="flex justify-end mr-8">
+            <NavLink
+              className={({ isActive }) =>
+                "font-palanquin font-semibold text-sm 2xl:text-xl xl:text-lg " +
+                (isActive ? "text-primary" : "text-white")
+              }
+              to={link.to}
+              onClick={() => setShowNav(false)}
+            >
+              {link.label}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
     </header>
   );
 };
