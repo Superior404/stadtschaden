67e1f742c23e3209554e1c1e94ab7f681a0e9ec7
Superior404 <93605138+Superior404@users.noreply.github.com>
2024-04-26 14:17:14 +0200

    prettier changes

--- frontend/stadtschaden/src/App.tsx
+++ frontend/stadtschaden/src/App.tsx
@@ -10,7 +10,7 @@ import NotFoundPage from "./components/screens/NotFoundPage";
 const App = () => {
   return (
     <BrowserRouter>
-      <Nav /> 
+      <Nav />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="instruction" element={<InstructionPage />} />
--- frontend/stadtschaden/src/components/common/FormInput.tsx
+++ frontend/stadtschaden/src/components/common/FormInput.tsx
@@ -1,18 +1,15 @@
-import React, { FC } from 'react'
-
-
+import React, { FC } from "react";
 
 export interface FormInputProps {
-    text: string;
+  text: string;
 }
-
 
 const FormInput: FC<FormInputProps> = (props) => {
   return (
     <div>
       <input type="text" title={props.text} />
     </div>
-  )
-}
+  );
+};
 
-export default FormInput
+export default FormInput;
