b8cb10f8e4eb7a3b0578b05ea3ff9f6d77bb8f34
Superior404 <93605138+Superior404@users.noreply.github.com>
2024-04-29 17:57:04 +0200

    inserted text above the input fields

--- frontend/stadtschaden/src/components/screens/ContactPage.tsx
+++ frontend/stadtschaden/src/components/screens/ContactPage.tsx
@@ -1,12 +1,37 @@
 import React from "react";
 import FormInput from "../common/FormInput";
 
+const styles = {
+  mainText: {
+    fontWeight: "bold",
+    color: "white",
+  },
+};
+
 const ContactPage = () => {
   return (
-    <div>
-      <div>ContactPage</div>
-      <FormInput text="Vorname" />
-      <FormInput text="Nachname" />
+    <div className="mt-36">
+      <div className="flex flex-col justify-center items-center font-montserrat font-bold text-4xl">
+        <p style={styles.mainText}>
+          Senden sie uns Ihre gefundenen{" "}
+          <span className="text-primary">Schäden!</span>
+        </p>
+        <p style={styles.mainText} className="">
+          <span className="text-primary">Wir </span>kümmern uns darum.
+        </p>
+        <p className="mt-5 text-base font-semibold text-center text-slate-gray w-auto text">
+          Für die Meldung von Schäden oder Verschmutzungen in der Stadt ist ein
+          präzises Ausfüllen des Formulars erforderlich.
+        </p>
+        <p className="mt-2 text-base font-semibold text-center text-slate-gray w-auto text">
+          Bitte geben Sie genaue Informationen an, einschließlich Ort und
+          Zeitpunkt des Vorfalls. Fotos sind obligatorisch.
+        </p>
+      </div>
+
+      <div>
+        <FormInput text="Vorname" />
+      </div>
     </div>
   );
 };
