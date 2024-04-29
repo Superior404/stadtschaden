1124f05f68a30004cebb994bbb81ab1ab89b9a84
Superior404 <93605138+Superior404@users.noreply.github.com>
2024-04-29 16:47:04 +0200

    test 2

--- frontend/stadtschaden/src/components/screens/ContactPage.tsx
+++ frontend/stadtschaden/src/components/screens/ContactPage.tsx
@@ -7,7 +7,7 @@ const ContactPage = () => {
       <div>ContactPage</div>
       <FormInput text="Vorname" />
       <FormInput text="Nachname" />
-      <p> test </p>
+      <p> test 2 </p>
     </div>
   );
 };
