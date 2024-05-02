85cd56c2fdaf9ec1fac13ceb3327e9bfaf295598
Superior404 <93605138+Superior404@users.noreply.github.com>
2024-05-02 22:43:29 +0200

    added about us section

--- frontend/stadtschaden/src/components/screens/LandingPage.tsx
+++ frontend/stadtschaden/src/components/screens/LandingPage.tsx
@@ -1,4 +1,5 @@
 import earthPicture from "../../assets/images/earth-picture.png";
+import teamPicture from "../../assets/images/team-picture.jpeg";
 import { TbArrowCurveRight, TbArrowCurveLeft } from "react-icons/tb";
 import { Link } from "react-router-dom";
 
@@ -12,8 +13,9 @@ const styles = {
 
 const LandingPage = () => {
   return (
-    <main className="h-full w-full bg-leafesBackground">
-      <div className="flex justify-center items-center h-screen">
+    <main>
+      {/* Das auslagern in eigene Komponente - LandingFolder */}
+      <div className="flex justify-center items-center h-screen w-full bg-leafesBackground">
         <div className="flex flex-row justify-center items-center">
           <p
             className="rotate-[-20deg] xl:text-3xl 2xl:text-6xl font-montserrat text-2xl text-center self-start mt-12"
@@ -47,6 +49,49 @@ const LandingPage = () => {
           </p>
         </div>
       </div>
+
+      {/* Das auslagern in eigene Komponente - AboutUsPage */}
+      <div className="flex flex-col justify-center h-[50vh] mt-20 mb-24 gap-8">
+        <p className="self-center font-palanquin text-primary font-bold text-4xl mb-8">
+          Über uns
+        </p>
+        <div className="flex flex-row items-center gap-6">
+          <img
+            src={teamPicture}
+            alt="teamPicture"
+            className="w-42 h-72 rounded-3xl ml-24 mr-12"
+          />
+
+          <div>
+            <p className="text-center text-ms font-semibold mr-24">
+              Willkommen bei{" "}
+              <span className="text-primary font-bold">StadtSchaden</span>
+              !<br />
+              <br />
+              Wir sind ein engagiertes Team, das sich leidenschaftlich für
+              unsere Umwelt und unsere Stadt einsetzt. Unser Ziel ist es, die
+              Schönheit und Sauberkeit unserer Stadt zu erhalten und zu
+              verbessern. Als Team von{" "}
+              <span className="text-primary font-bold">StadtSchaden</span>{" "}
+              setzen wir uns dafür ein, Schäden wie{" "}
+              <span className="text-primary font-bold">Graffiti</span>,{" "}
+              <span className="text-primary font-bold">Müll</span> und
+              Vandalismus zu beseitigen und präventive Maßnahmen zu ergreifen,
+              um zukünftige Schäden zu verhindern. <br />
+              <br /> Unsere Arbeit konzentriert sich auf Nachhaltigkeit durch
+              den Einsatz umweltfreundlicher Materialien und Methoden, um unsere
+              Stadt sauber zu halten. Mit
+              <span className="text-primary font-bold">StadtSchaden</span>{" "}
+              können Sie Schäden melden und wir helfen Ihnen, diese zu
+              beseitigen und Maßnahmen zur Prävention zu ergreifen. Schließen
+              Sie sich unserem Team an, um gemeinsam eine nachhaltige und
+              saubere Umgebung zu schaffen.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center h-screen w-full bg-leafesBackground"></div>
     </main>
   );
 };
--- frontend/stadtschaden/src/constants/NavLinks.ts
+++ frontend/stadtschaden/src/constants/NavLinks.ts
@@ -1,8 +1,8 @@
 export const navLinks = [
   { to: "", label: "Home" },
+  { to: "about-us", label: "Über uns" },
+  { to: "news", label: "News" },
   { to: "instruction", label: "Anleitung" },
   { to: "contact", label: "Melden" },
-  { to: "news", label: "News" },
-  { to: "about-us", label: "Über uns" },
   { to: "worker", label: "Mitarbeiter" },
 ];
