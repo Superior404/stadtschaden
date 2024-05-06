4dc828fa8b8d3318ea50b903c6983d2782d7d92b
Superior404 <93605138+Superior404@users.noreply.github.com>
2024-05-06 13:21:58 +0200

    prettier fix

--- frontend/stadtschaden/src/components/screens/ContactPage.tsx
+++ frontend/stadtschaden/src/components/screens/ContactPage.tsx
@@ -163,7 +163,7 @@ const ContactPage = () => {
 
           <SelectButton
             options={streetDamageCategories.map(
-              (category) => category.category
+              (category) => category.category,
             )}
             value={category}
             onChange={(event) => setCategory(event.target.value)}
