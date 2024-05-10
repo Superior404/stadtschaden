ec31236419b5e40a6be60bf8bc87afd707bc9865
Superior404 <93605138+Superior404@users.noreply.github.com>
2024-05-10 15:32:24 +0200

    news section ajudsted

--- frontend/stadtschaden/src/components/sections/NewsSection.tsx
+++ frontend/stadtschaden/src/components/sections/NewsSection.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import streetDamagePicture from "../../assets/images/streetDamages/streetdamage2.jpg.webp";
 
 const NewsSection = () => {
   return (
-    <>
+    <div>
       <p className="font-palanquin text-black font-bold text-4xl mt-12">
         Neuigkeiten
       </p>
-    </>
+
+      <div>
+
+      </div>
+    </div>
   );
 };
 
