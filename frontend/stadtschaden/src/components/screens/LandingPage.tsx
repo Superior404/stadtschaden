80c8020e54030f09e0c0106745492cf6291a456f
Superior404 <93605138+Superior404@users.noreply.github.com>
2024-06-14 16:46:17 +0200

    arrows on landing page are also responsive

--- frontend/stadtschaden/src/components/screens/LandingPage.tsx
+++ frontend/stadtschaden/src/components/screens/LandingPage.tsx
@@ -9,7 +9,6 @@ import AboutUsSection from "../sections/AboutUsSection";
 import NewsSection from "../sections/NewsSection";
 import Footer from "../common/Footer";
 
-// TODO Alessio: Make this page (and further pages) responsive ..
 const styles = {
   mainText: {
     fontWeight: "bold",
@@ -20,33 +19,33 @@ const styles = {
 const LandingPage = () => {
   return (
     <main>
-      {/* Das auslagern in eigene Komponente - LandingFolder */}
+      {/* LandingPage */}
       <div className="flex justify-center items-center bg-center bg-cover bg-lightLeafesBackground">
-        <div className="flex flex-col justify-center items-center lg:flex-row h-screen ">
+        <div className="flex flex-col justify-center items-center md:flex-row h-screen ">
           <p
-            className="rotate-[-0deg] xl:text-4xl 2xl:text-6xl font-montserrat text-black text-2xl text-end self-start mt-36 hidden lg:block"
+            className="rotate-[-0deg] xl:text-4xl lg:text-3xl md:text-2xl sm:text-2xl text-xl font-montserrat text-black text-end self-start lg:mt:36 md:mt-48  hidden md:block"
             style={styles.mainText}
           >
             Melde einen
             <br /> Schaden!
           </p>
 
-          <TbArrowCurveRight className="text-black -m-6 size-48 rotate-[10deg] lg:rotate-[110deg] hidden md:block" />
+          <TbArrowCurveRight className="text-black -m-6 size-48 rotate-[10deg] md:rotate-[110deg] hidden md:block" />
 
           <div className="flex justify-center items-center">
             <Link to="/contact">
               <img
                 src={earthPicture}
                 alt="EarthPicture"
-                className="max-w-xs 2xl:max-w-lg xl:max-w-md lg:mt-15 cursor-pointer transition duration-200 transform hover:scale-105"
+                className="max-w-xs 2xl:max-w-lg xl:max-w-md md:mt-15 cursor-pointer transition duration-200 transform hover:scale-105"
               />
             </Link>
           </div>
 
-          <TbArrowCurveLeft className="text-black -m-6 size-48 rotate-[-10deg] lg:rotate-[-110deg] hidden md:block" />
+          <TbArrowCurveLeft className="text-black -m-6 size-48 rotate-[-10deg] md:rotate-[-110deg] hidden md:block" />
 
           <p
-            className="rotate-[0deg] xl:text-4xl 2xl:text-6xl font-montserrat text-2xl text-center self-start mt-36 hidden lg:block"
+            className="rotate-[-0deg] xl:text-4xl lg:text-3xl md:text-2xl sm:text-2xl text-xl font-montserrat text-black text-end self-start lg:mt:36 md:mt-48  hidden md:block"
             style={styles.mainText}
           >
             Rette deine
